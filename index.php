90020a5ea77322a77a8c094384c8713cde602ae4
arturwarlord <69643781+arturwarlord@users.noreply.github.com>
2024-05-10 16:02:26 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,472 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Страница о шахматах</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&display=swap" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css2?family=Golos+Text:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css?v=2">
+</head>
+<body>
+<header>
+    <div class=""></div>
+</header>
+
+    <main>
+<section class="content">
+        <div class="container-content">
+           <div class="header">
+            <img src="assets/img/logo.png" alt="Логотип Шахматы">
+            </div>
+    <div class="text">
+        <div class="slogan">
+            <h2>Превратите<br/>уездный город</h2>
+            <h2 class="center">В сталицу <br/>земного шара</h2>
+            <p>Оплатите взнос на телеграммы для организации Международного васюкинского турнира по шахматам</p>
+        </div>
+        <div class="buttons">
+            <a class="default" href="#" onclick="smoothScroll('support')"><p>Поддержать шахматную мысль</p></a>
+            <a href="#" onclick="smoothScroll('players')"><p>Подробнее о турнире</p></a>
+        </div>
+    </div>
+    <aside class="image">
+        <img src="assets/img/back.png" alt="Изображение шахмат">
+    </aside>
+    </div>
+    </section>
+<div class="container-line">
+            <div class="running_line">
+        <span class="text-run">Дело помощи утопающим — дело рук самих утопающих! <span class="circle"></span> Шахматы двигают вперед не только культуру, но и экономику! <span class="circle"></span> Лед тронулся, господа присяжные заседатели! <span class="circle"></span> Будущие источники обогащения васюкинцев</span>
+    </div>
+       </div>
+
+<section class="support" id="support">
+   <div class="support-container">
+    <div class="support-top">
+        <div class="support-text">
+            <p>Чтобы поддержать <br/>Международный васюкинский турнир посетите лекцию на тему:<br/> <span>«Плодотворная дебютная идея»</span></p>
+        </div>
+        <div class="support-image">
+            <img src="assets/img/people.png" alt="Изображение">
+        </div>
+    </div>
+
+    <div class="support-bottom">
+       <div class="mobile_support-text"><p>и Сеанс <span>одновременной игры в шахматы на 160 досках</span> гроссмейстера О. Бендера</p></div>
+
+        <div class="support-image">
+            <img src="assets/img/person.png" alt="Изображение">
+        </div>
+
+        <div class="support-text">
+            <p>и Сеанс <span>одновременной игры в шахматы на 160 досках</span> гроссмейстера О. Бендера</p>
+            <table>
+    <tr>
+        <td>Место проведения:</td>
+        <td>Клуб "Картонажик"</td>
+    </tr>
+    <tr>
+        <td>Дата и время мероприятия:</td>
+        <td>22 июня 1927г. в 18:00</td>
+    </tr>
+    <tr>
+        <td>Стоимость входных билетов:</td>
+        <td>20 коп</td>
+    </tr>
+    <tr>
+        <td>Плата за игру:</td>
+        <td>50 коп</td>
+    </tr>
+    <tr>
+        <td>Взнос на телеграммы:</td>
+        <td><b style="text-decoration: line-through; text-decoration-color: red;">100 руб </b> 21 руб. 16 коп.</td>
+    </tr>
+</table>
+<a href="">По всем вопросам обращаться в администрацию к К. Михельсону</a>
+        </div>
+    </div>
+    </div>
+</section>
+
+<section class="container-stages">
+    <h2>Этапы преображения <br/>Васюкиных<p class="pre-line">Будущие источники обогащения<br/>васюкинцев</p></h2>
+
+    <div class="transformation-stages">
+    <div class="card">
+        <div class="number">1</div>
+        <div class="stages_text">Строительство железнодорожной магистрали Москва-Васюки</div>
+    </div>
+    <div class="card">
+        <div class="number">2</div>
+        <div class="stages_text">Открытие фешенебельной гостиницы «Проходная пешка» и других небоскрёбов</div>
+    </div>
+    <div class="card">
+        <div class="number">3</div>
+        <div class="stages_text">Поднятие сельского хозяйства в радиусе на тысячу километров: производство овощей, фруктов, икры, шоколадных конфет</div>
+    </div>
+    <div class="card">
+        <div class="number">4</div>
+        <div class="stages_text">Строительство дворца для турнира</div>
+    </div>
+    <div class="card">
+        <div class="number">5</div>
+        <div class="stages_text">Размещение гаражей для гостевого автотранспорта</div>
+    </div>
+    <div class="card">
+        <div class="number">6</div>
+        <div class="stages_text">Постройка сверхмощной радиостанции для передачи всему миру сенсационных результатов</div>
+    </div>
+    <div class="card">
+        <div class="number">7</div>
+        <div class="stages_text stages_text--over">Создание аэропорта «Большие Васюки» с регулярным отправлением почтовых самолётов и дирижаблей во все концы света, включая Лос-Анжелос и Мельбурн
+        </div>
+        <img src="assets/img/fly.png" alt="самолет">
+    </div>
+</div>
+
+    <div class="transformation-stages-mobile-slider">
+          <div class="transformation-stages-mobile-slide active">
+            <div class="card card--one">
+                <div class="number">1</div>
+                <div class="stages_text">Строительство железнодорожной магистрали Москва-Васюки</div>
+            </div>
+            <div class="card card--two">
+                <div class="number">2</div>
+                <div class="stages_text">Открытие фешенебельной гостиницы «Проходная пешка» и других небоскрёбов</div>
+            </div>
+</div>
+            <img src="assets/img/fly.png" alt="самолет">
+
+          <div class="transformation-stages-mobile-slide">
+            <div class="card card--three">
+                <div class="number">3</div>
+                <div class="stages_text">Поднятие сельского хозяйства в радиусе на тысячу километров: производство овощей, фруктов, икры, шоколадных конфет</div>
+            </div>
+          </div>
+
+           <div class="transformation-stages-mobile-slide">
+                <div class="card card--four">
+                    <div class="number">4</div>
+                    <div class="stages_text">Строительство дворца для турнира</div>
+                </div>
+                <div class="card card--five">
+                    <div class="number">5</div>
+                    <div class="stages_text">Размещение гаражей для гостевого автотранспорта</div>
+                </div>
+           </div>
+
+        <div class="transformation-stages-mobile-slide">
+            <div class="card card--three">
+                <div class="number">6</div>
+                <div class="stages_text">Постройка сверхмощной радиостанции для передачи всему миру сенсационных результатов</div>
+            </div>
+        </div>
+
+        <div class="transformation-stages-mobile-slide">
+            <div class="card card--three">
+                <div class="number">7</div>
+                <div class="stages_text stages_text--over">Создание аэропорта «Большие Васюки» с регулярным отправлением почтовых самолётов и дирижаблей во все концы света, включая Лос-Анжелос и Мельбурн
+                </div>
+            </div>
+        </div>
+
+    </div>
+<div class="navigation-mobile">
+        <div class="nav_page-mobile">
+            <button onclick="oldSlideMobile()" class="prev-button">&lt;</button>
+            <div class="page-circle"></div>
+            <button class="next-button" onclick="newSlideMobile()">&gt;</button>
+        </div>
+    </div>
+</section>
+
+<section class="participants-container" id="players">
+    <div class="navigation">
+        <h2>Участники турнира</h2>
+        <div class="nav_page">
+            <button onclick="prevSlide()" class="prev-button">&lt;</button>
+            <div class="page-indicator"><span>1</span>/<snap>2</snap></div>
+            <button class="next-button" onclick="nextSlide()">&gt;</button>
+        </div>
+    </div>
+    <div class="participants-cards_slider">
+       <div class="participants-cards_slider-list active">
+        <div class="participants-cards">
+        <div class="participant-card">
+            <img src="assets/img/player.png" alt="Participant 1">
+            <h3>Хозе-Рауль Капабланка</h3>
+            <p>Чемпион мира по шахматам</p>
+            <button>Подробнее</button>
+        </div>
+        <div class="participant-card">
+            <img src="assets/img/player.png" alt="Participant 2">
+            <h3>Эммануил Ласкер</h3>
+            <p>Чемпион мира по шахматам</p>
+            <button>Подробнее</button>
+        </div>
+        <div class="participant-card">
+            <img src="assets/img/player.png" alt="Participant 3">
+            <h3>Александр Алехин</h3>
+            <p>Чемпион мира по шахматам</p>
+            <button>Подробнее</button>
+        </div>
+        </div>
+        </div>
+        <div class="participants-cards_slider-list">
+        <div class="participants-cards">
+        <div class="participant-card">
+            <img src="assets/img/player.png" alt="Participant 1">
+            <h3>Арон Нимцович</h3>
+            <p>Чемпион мира по шахматам</p>
+            <button>Подробнее</button>
+        </div>
+        <div class="participant-card">
+            <img src="assets/img/player.png" alt="Participant 2">
+            <h3>Рихард Рети</h3>
+            <p>Чемпион мира по шахматам</p>
+            <button>Подробнее</button>
+        </div>
+        <div class="participant-card">
+            <img src="assets/img/player.png" alt="Participant 3">
+            <h3>Остап Бендер</h3>
+            <p>Чемпион мира по шахматам</p>
+            <button>Подробнее</button>
+        </div>
+        </div>
+        </div>
+    </div>
+
+<div class="participants-cards_slider-mobile">
+    <h2>Участники турнира</h2>
+    <div class="participant-card participant-card--mobile active">
+        <img src="assets/img/player.png" alt="Participant 1">
+        <h3>Хозе-Рауль Капабланка</h3>
+        <p>Чемпион мира по шахматам</p>
+        <button>Подробнее</button>
+    </div>
+    <div class="participant-card participant-card--mobile">
+        <img src="assets/img/player.png" alt="Participant 2">
+        <h3>Эммануил Ласкер</h3>
+        <p>Чемпион мира по шахматам</p>
+        <button>Подробнее</button>
+    </div>
+    <div class="participant-card participant-card--mobile">
+        <img src="assets/img/player.png" alt="Participant 3">
+        <h3>Александр Алехин</h3>
+        <p>Чемпион мира по шахматам</p>
+        <button>Подробнее</button>
+    </div>
+    <div class="participant-card participant-card--mobile">
+        <img src="assets/img/player.png" alt="Participant 1">
+        <h3>Арон Нимцович</h3>
+        <p>Чемпион мира по шахматам</p>
+        <button>Подробнее</button>
+    </div>
+    <div class="participant-card participant-card--mobile">
+        <img src="assets/img/player.png" alt="Participant 2">
+        <h3>Рихард Рети</h3>
+        <p>Чемпион мира по шахматам</p>
+        <button>Подробнее</button>
+    </div>
+    <div class="participant-card participant-card--mobile">
+        <img src="assets/img/player.png" alt="Participant 3">
+        <h3>Остап Бендер</h3>
+        <p>Чемпион мира по шахматам</p>
+        <button>Подробнее</button>
+    </div>
+</div>
+
+<div class="participants-navigation-mobile">
+    <div class="nav_page">
+        <button onclick="lastSlideMobile()" class="prev-button">&lt;</button>
+        <div class="page-id"></div>
+        <button class="next-button" onclick="futureSlideMobile()">&gt;</button>
+    </div>
+</div>
+
+</section>
+<div class="container-line">
+            <div class="running_line">
+        <span class="text-run">Дело помощи утопающим — дело рук самих утопающих! <span class="circle"></span> Шахматы двигают вперед не только культуру, но и экономику! <span class="circle"></span> Лед тронулся, господа присяжные заседатели! <span class="circle"></span> Будущие источники обогащения васюкинцев</span>
+    </div>
+       </div>
+    </main>
+   <script>
+    const slider = document.querySelector('.participants-cards_slider');
+    const slides = document.querySelectorAll('.participants-cards_slider-list');
+    const slideWidth = document.querySelector('.participants-cards_slider-list').clientWidth;
+    let slideIndex = 0;
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            if (i === index) {
+                slide.classList.add('active');
+            } else {
+                slide.classList.remove('active');
+            }
+        });
+    }
+
+    function updatePageIndicator() {
+        const currentPage = slideIndex + 1;
+        const totalPages = slides.length;
+        document.querySelector('.page-indicator').textContent = `${currentPage} / ${totalPages}`;
+    }
+
+    function nextSlide() {
+        if (slideIndex < slides.length - 1) {
+            slideIndex++;
+        } else {
+            slideIndex = 0;
+        }
+        showSlide(slideIndex);
+        updatePageIndicator();
+    }
+
+    function prevSlide() {
+        if (slideIndex > 0) {
+            slideIndex--;
+        } else {
+            slideIndex = slides.length - 1;
+        }
+        showSlide(slideIndex);
+        updatePageIndicator();
+    }
+
+    showSlide(slideIndex);
+    updatePageIndicator();
+
+       function startAutoSlide() {
+        slideInterval = setInterval(nextSlide, 4000); // Переключение каждые 2 секунды
+    }
+
+    function stopAutoSlide() {
+        clearInterval(slideInterval);
+    }
+
+    startAutoSlide(); // Начать автоматическое переключение слайдов
+
+</script>
+   <script>
+    const sliderMobile = document.querySelector('.transformation-stages-mobile-slider');
+    const slidesMobile = document.querySelectorAll('.transformation-stages-mobile-slide');
+    const slideWidthMobile = document.querySelector('.transformation-stages-mobile-slide').clientWidth;
+    let slideIndexMobile = 0;
+
+    function pokazSlideMobile(index) {
+        slidesMobile.forEach((slide, i) => {
+            if (i === index) {
+                slide.classList.add('active');
+            } else {
+                slide.classList.remove('active');
+            }
+        });
+    }
+
+   function updatePageIndiMobile() {
+    const totalPages = slidesMobile.length;
+    const pageCircle = document.querySelector('.page-circle');
+    pageCircle.innerHTML = ''; // Очищаем содержимое элемента
+    for (let i = 0; i < totalPages; i++) {
+        const dot = document.createElement('span');
+        dot.innerHTML = '&#9679;'; // Добавляем символ точки
+        dot.classList.add('dot'); // Добавляем класс "dot" к каждой точке
+        if (i === slideIndexMobile) {
+            dot.classList.add('active'); // Добавляем класс "active" к активной точке
+        }
+        pageCircle.appendChild(dot); // Добавляем точку внутрь элемента pageCircle
+    }
+}
+
+    function newSlideMobile() {
+        if (slideIndexMobile < slidesMobile.length - 1) {
+            slideIndexMobile++;
+        } else {
+            slideIndexMobile = 0;
+        }
+        pokazSlideMobile(slideIndexMobile);
+        updatePageIndiMobile();
+    }
+
+    function oldSlideMobile() {
+        if (slideIndexMobile > 0) {
+            slideIndexMobile--;
+        } else {
+            slideIndexMobile = slidesMobile.length - 1;
+        }
+        pokazSlideMobile(slideIndexMobile);
+        updatePageIndiMobile();
+    }
+
+    pokazSlideMobile(slideIndexMobile);
+    updatePageIndiMobile();
+</script>
+<script>
+    const sliderMobilePart = document.querySelector('.participants-cards_slider-mobile');
+    const slidesMobilePart = document.querySelectorAll('.participant-card--mobile');
+    let slideIndexMobilePart = 0;
+
+    function showSlideMobilePart(index) {
+        slidesMobilePart.forEach((slide, i) => {
+            if (i === index) {
+                slide.classList.add('active');
+            } else {
+                slide.classList.remove('active');
+            }
+        });
+    }
+
+    function updatePageIndicatorMobilePart() {
+        const currentPage = slideIndexMobilePart + 1;
+        const totalPages = slidesMobilePart.length;
+        document.querySelector('.page-id').textContent = `${currentPage} / ${totalPages}`;
+    }
+
+    function futureSlideMobile() {
+        if (slideIndexMobilePart < slidesMobilePart.length - 1) {
+            slideIndexMobilePart++;
+        } else {
+            slideIndexMobilePart = 0;
+        }
+        showSlideMobilePart(slideIndexMobilePart);
+        updatePageIndicatorMobilePart();
+    }
+
+    function lastSlideMobile() {
+        if (slideIndexMobilePart > 0) {
+            slideIndexMobilePart--;
+        } else {
+            slideIndexMobilePart = slidesMobilePart.length - 1;
+        }
+        showSlideMobilePart(slideIndexMobilePart);
+        updatePageIndicatorMobilePart();
+    }
+
+    if (slidesMobilePart.length > 0) {
+        showSlideMobilePart(slideIndexMobilePart);
+        updatePageIndicatorMobilePart();
+    }
+</script>
+    <footer>
+        <p>Все персонажи, события и цитаты являются вымышленными и не принадлежат создателям сайта. С подробностями можно познакомиться в главе XXXIV романа Ильи Ильфа и Евгения Петрова «Двенадцать стульев».</p>
+    </footer>
+
+    <script>
+    // Функция для плавной прокрутки до элемента с указанным id
+    function smoothScroll(targetId) {
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            // Вычисляем расстояние до элемента
+            const offset = targetElement.offsetTop;
+            // Плавно прокручиваем страницу до элемента
+            window.scrollTo({
+                top: offset,
+                behavior: 'smooth' // Добавляем плавность
+            });
+        }
+    }
+</script>
+</body>
+</html>
